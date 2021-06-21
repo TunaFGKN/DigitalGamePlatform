7368f2120cc43a3f114372675990684c9c1e0f0d
TunaFigankaplan <swaxztn@gmail.com>
2021-06-21 13:49:06 +0300

    Fluent Validation

---
+++ Business/ValidationRules/FluentValidation/GameValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class GameValidator: AbstractValidator<Game>
+    {
+        public GameValidator()
+        {
+            RuleFor(g => g.GameName).NotEmpty();
+            RuleFor(g => g.GameName).MinimumLength(2);
+            RuleFor(g => g.GamePrice).NotEmpty();
+            RuleFor(g => g.GamePrice).GreaterThan(0);
+        }
+    }
+}
